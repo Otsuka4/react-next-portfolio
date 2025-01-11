8dfeda216e0b7d775a50c6c32ad4be350796da90
Otsuka4 <ktc24a31e0003@edu.kyoto-tech.ac.jp>
2025-01-11 16:28:32 +0900

    top文字

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+//members = プロフィールページ
+
 import styles from './page.module.css'
 import Image from 'next/image'
 
@@ -16,9 +18,9 @@ export default async function Home() {
     <>
       <section className={styles.top}>
         <div>
-          <h1 className={styles.title}>テクノロジーの力で世界を変える</h1>
+          <h1 className={styles.title}>Portfolio</h1>
           <p className={styles.description}>
-            私たちは市場をリードしているグローバルテックカンパニーです。
+            これはああああのポートフォリオです。
           </p>
         </div>
         <Image
