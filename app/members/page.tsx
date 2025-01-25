77f39777f9398e43b1714dbfd72a3b7bf8f2e19b
Otsuka4 <ktc24a31e0003@edu.kyoto-tech.ac.jp>
2025-01-25 16:00:29 +0900

    自己紹介

--- app/members/page.tsx
+++ app/members/page.tsx
@@ -6,7 +6,7 @@ import { Key, ReactElement, JSXElementConstructor, ReactNode, ReactPortal, Await
 
 export default async function Page() {
   // microCMSからブログ投稿を取得
-const blogPosts = await getMembersList({ limit : MEMBERS_LIST_LIMIT});
+const blogPosts = await getMembersList();
 
   return (
     <div className={styles.container}>
@@ -24,9 +24,31 @@ const blogPosts = await getMembersList({ limit : MEMBERS_LIST_LIMIT});
           <div className={styles.introText}>
             <p>
               こんにちは！私は　大塚 由華　です。<br />
-              このページでは私が学んでいることや趣味を紹介しています。<br />
-              私は現在、専門学校で情報セキュリティについて学んでいます。<br />
+              このページでは自己紹介や私の趣味を紹介しています。<br />
               どうぞよろしくお願いします！<br />
+            </p>
+
+            <h2>自己紹介</h2>
+            <p>
+              私は、滋賀県出身の専門学生です。<br />
+              専門学校では情報セキュリティを学んでいます。<br />
+              <br />
+            </p>
+
+            <h2>学んでいること</h2>
+            <p>
+              現在、専門学校で情報セキュリティについて学んでいます。<br />
+              セキュリティについて学ぶきっかけは、小学生の時、友達の親が乗っ取りにあい、私の家にもカードを買ってきて欲しい。
+              とメールが届き、とても怖い思いをしたからです。<br />
+              今後は、セキュリティの知識を活かして、被害を少しでも減らす仕事をしたいと思っています。<br />
+            </p>
+
+            <h2>趣味</h2>
+            <p>
+              趣味は、読書とゲーム、音楽を聴くことです。<br />
+              特に、ミステリー小説や長編小説が好きです。<br />
+              ゲームはホヨバースのゲーム、音楽はボカロやバンドなど色々聴いています。<br />
+              おすすめの物があればぜひ教えてください！<br />
             </p>
           </div>
         </div>
